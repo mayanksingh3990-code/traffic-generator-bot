a1d4f8bec1928318d772100f200168846ada8b22
mayanksingh3990-code <mayanksingh3990@gmail.com>
2025-11-29 19:58:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 require("./server.js");
 require("./bot.js");
+
+const puppeteer = require('puppeteer');
+const path = require('path');
+
+(async () => {
+  // Launch Puppeteer for headless browsing on Render/Glitch
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
+  const page = await browser.newPage();
+
+  // Open your local index.html from the 'public' folder
+  const filePath = path.join(__dirname, 'public', 'index.html');
+  await page.goto(`file://${filePath}`);
+
+  // Your bot logic goes here
+})();
